ea48c629d9da8896c6dd2e28122740d2cbf4712e
LH <liam.howe2@education.nsw.gov.au>
2024-11-19 16:56:29 +1100

    Changed images and some titles

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,11 @@
+const act = "public/images/act_map.png"
+const nsw = "public/images/nsw_map.png"
+const vic = "public/images/vic_map.png"
+const tas = "public/images/tas_map.png"
+const qld = "public/images/qld_map.png"
+const sa = "public/images/sa_map.png"
+const nt = "public/images/nt_map.png"
+const wa = "public/images/wa_map.png"
 let result = "";
 fetch("./frontEndData.json")
     .then(function (response) {
@@ -11,10 +19,28 @@ fetch("./frontEndData.json")
     });
 function appendData(data) {
     data.forEach(({ Date, HolidayName, Information, MoreInformation, Jurisdiction } = rows) => {
+        var JurisdictionImage
+        if (Jurisdiction == "act") {
+            JurisdictionImage = "images/act_map.png";
+        }   else if (Jurisdiction == "nsw") {
+            JurisdictionImage = "images/nsw_map.png";
+        }   else if (Jurisdiction == "vic") {
+            JurisdictionImage = "images/vic_map.png";
+        }   else if (Jurisdiction == "tas") {
+            JurisdictionImage = "images/tas_map.png";
+        }   else if (Jurisdiction == "qld") {
+            JurisdictionImage = "images/qld_map.png";
+        }   else if (Jurisdiction == "sa") {
+            JurisdictionImage = "images/sa_map.png";
+        }   else if (Jurisdiction == "nt") {
+            JurisdictionImage = "images/nt_map.png";
+        }   else if (Jurisdiction == "wa") {
+            JurisdictionImage = "images/wa_map.png";
+        }
         result += `
             <div class="card">
-            <img class="card-image" src="${HolidayName}" alt="Product image for the ${Date} VSCode extension."/>
-            <h1 class="card-name">${Date}</h1>
+            <img class="card-image" src=${JurisdictionImage} alt="Product image for the ${Date} VSCode extension."/>
+            <h1 class="card-name">${HolidayName}</h1>
             <p class="card-about">${MoreInformation}</p>
             <a class="card-link" href="${Information}"><button class="btn">Read More</button></a>
             </div>

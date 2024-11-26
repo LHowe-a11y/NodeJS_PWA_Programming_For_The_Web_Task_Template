10526e2803448458ac55e6f782bfe456390a931d
LH <liam.howe2@education.nsw.gov.au>
2024-11-26 20:37:37 +1100

    Created a search input bar for the earliest date, invalid input warning, latest date bar will be next reusing a lot of the same code.

--- newpage/js/content.js
+++ newpage/js/content.js
@@ -1,14 +1,19 @@
 // const sqlite3 = require("sqlite3").verbose();
 // const db = new sqlite3.Database(".database/data_source.db");
 
-let act = true
-let nsw = true 
-let vic = true
-let tas = true
-let qld = true
-let sa = true
-let nt = true
-let wa = true
+let number = Number(1)
+
+let act = true;
+let nsw = true;
+let vic = true;
+let tas = true;
+let qld = true;
+let sa = true;
+let nt = true;
+let wa = true;
+
+let earliestDateBar = document.getElementById("earliestDate");
+let earlyDateWarningShown = false;
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -36,72 +41,95 @@ function sortDrop() {
 
 function actFilter() {
     if (act) {
-        act = false
+        act = false;
     } else if (!act) {
-        act = true
+        act = true;
     }
     document.getElementById("actButton").classList.toggle("toggledon");
 }
 
 function nswFilter() {
     if (nsw) {
-        nsw = false
+        nsw = false;
     } else if (!nsw) {
-        nsw = true
+        nsw = true;
     }
     document.getElementById("nswButton").classList.toggle("toggledon");
 }
 
 function vicFilter() {
     if (vic) {
-        vic = false
+        vic = false;
     } else if (!vic) {
-        vic = true
+        vic = true;
     }
     document.getElementById("vicButton").classList.toggle("toggledon");
 }
 
 function tasFilter() {
     if (tas) {
-        tas = false
+        tas = false;
     } else if (!tas) {
-        tas = true
+        tas = true;
     }
     document.getElementById("tasButton").classList.toggle("toggledon");
 }
 
 function qldFilter() {
     if (qld) {
-        qld = false
+        qld = false;
     } else if (!qld) {
-        qld = true
+        qld = true;
     }
     document.getElementById("qldButton").classList.toggle("toggledon");
 }
 
 function saFilter() {
     if (sa) {
-        sa = false
+        sa = false;
     } else if (!sa) {
-        sa = true
+        sa = true;
     }
     document.getElementById("saButton").classList.toggle("toggledon");
 }
 
 function ntFilter() {
     if (nt) {
-        nt = false
+        nt = false;
     } else if (!nt) {
-        nt = true
+        nt = true;
     }
     document.getElementById("ntButton").classList.toggle("toggledon");
 }
 
 function waFilter() {
     if (wa) {
-        wa = false
+        wa = false;
     } else if (!wa) {
-        wa = true
+        wa = true;
     }
     document.getElementById("waButton").classList.toggle("toggledon");
+}
+
+function earlyDateBar() {
+    let earlyDateInput = earliestDateBar.value;
+    // document.getElementById("invalidWarningCapsule").classList.toggle("show");
+    console.log(typeof Number(earlyDateInput))
+    let earlyDateNumber = Number(earlyDateInput)
+    // console.log(typeof earlyDateNumber);
+    if (earlyDateWarningShown == false) {
+        // document.getElementById("invalidWarningCapsule").classList.toggle("show");
+        if (earlyDateNumber < 10000000 || earlyDateNumber > 100000000) {
+            earlyDateWarningShown = true;
+            document.getElementById("invalidWarningCapsule").classList.toggle("show");
+        } else if (earlyDateInput.length != 8) {
+            earlyDateWarningShown = true;
+            document.getElementById("invalidWarningCapsule").classList.toggle("show");
+        }
+    } else if (earlyDateWarningShown) {
+        if (earlyDateNumber > 10000000 && earlyDateNumber < 100000000 && earlyDateInput.length == 8) {
+            earlyDateWarningShown = false;
+            document.getElementById("invalidWarningCapsule").classList.toggle("show");
+        }
+    }
 }
